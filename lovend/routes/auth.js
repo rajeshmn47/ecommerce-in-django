348a1416f4f03c446b62bab28af55fde1646586c
rajeshmn47 <rajeshmn47@gmail.com>
2022-01-08 21:18:46 +0530

    first commit

---
+++ lovend/routes/auth.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  console.log(req.body)
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    
+    const user = await User.findOne({ username: req.body.username });
+ 
+    !user && res.status(401).json("Wrong credentials!");
+    console.log('ncert')
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    console.log('ncert')
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+      console.log('cbse')
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {expiresIn:"3d"}
+    );
+    console.log(user._doc)
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, accessToken});
+  } catch (err) {
+    console.log('jjjjjjj')
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
